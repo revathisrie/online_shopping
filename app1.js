4269360de6dd0556d93ee5f700147b2b2986545f
REVATHI MYLAVARAPU <revathisrie@gmail.com>
2021-04-22 16:58:04 -0700

    adding app1.js to GitHub

--- app1.js
+++ app1.js
@@ -1,3 +1,38 @@
-let deals = prompt('Do you want deals? Say "yes"/"no".');
+        
+        promptInput = getPromptInput();
+        console.log(promptInput);
+        deals(promptInput)
 
-document.write('<h2>You chose ' + deals + ' to deals.</h2>');
+        function getPromptInput()
+        {
+        let promptInput = prompt('Do you want deals? Say "yes"/"no".');
+        // Validation.
+        // Re-prompt if input is not valid.
+        while(promptInput != 'yes' && promptInput != 'no') {
+            promptInput = prompt('Sorry INVALID INPUT. Do you want deals? Say "yes"/"no".');
+        }
+        return promptInput;
+        }
+
+
+        function deals(dealsInput)
+        {
+            document.write('<h2>You chose ' + dealsInput + ' to deals.</h2>');
+            console.log(dealsInput)
+        if (dealsInput == 'yes'){
+            document.write('<h3>Hurray..!! you chose yes to deals.</h3>');
+        }else if(dealsInput == 'no') {
+            document.write('<h3>Oops!!! You missed on potential deals.</h3>');
+        } else {
+            document.write('<h3>Choose yes in the pop up for great deals..!!:)</h3>');
+        } 
+        }
+
+        function addPictures(){
+            numberOfDeals = prompt('How many deals you want to see.?');
+            url ="https://image.freepik.com/free-vector/online-shopping-with-woman-character_1133-387.jpg"
+            for(let i=0; i<numberOfDeals; i++)
+            {
+                document.write("<img src=" + url + " >");
+            }
+        }
