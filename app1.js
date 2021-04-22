6b3e08ee190d025244712c10e9fa9075ba8be996
REVATHI MYLAVARAPU <revathisrie@gmail.com>
2021-04-21 22:00:51 -0700

    Adding Conditional JavaScript popup

---
+++ app1.js
@@ -0,0 +1,3 @@
+let deals = prompt('Do you want deals? Say "yes"/"no".');
+
+document.write('<h2>You chose ' + deals + ' to deals.</h2>');
---
+++ app2.js
@@ -0,0 +1,8 @@
+console.log(deals)
+if (deals.toLowerCase() == 'yes'){
+    document.write('<h3>Hurray..!! you chose yes to deals.</h3>');
+}else if(deals.toLowerCase() == 'no') {
+    document.write('<h3>Oops..!! You missed on potential deals.l.</h3>');
+} else {
+    document.write('<h3>Choose yes in the pop up for great deals..!!:)</h3>');
+} 
